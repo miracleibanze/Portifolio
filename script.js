2fee222cdd92a8e72ba30fe6904fc47c17b3bf39
miracleibanze <miraclecode11@gmail.com>
2024-06-05 12:34:35 +0200

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+document.querySelector('.button').addEventListener('click', () => {
+  turnSections(['.main-right', '.main-right2']);
+});
+
+function turnSections(sections) {
+  sections.forEach((selector, index) => {
+    setTimeout(() => {
+      document.querySelector(selector).classList.add('turn');
+    }, index * 500);
+  });
+}
+
 document.querySelectorAll('.Next').forEach(button => {
   button.addEventListener('click', () => {
     const mainRight = button.closest('.main-right, .main-right2, .main-right3, .main-right4');
     mainRight.classList.add('turn');
   });
 });
+
+let start = document.querySelector('.start');
+start.addEventListener('click', () => {
+    const mainRight = document.querySelector('.main-right');
+    mainRight.classList.add('turn');
+  });
 
 document.querySelectorAll('.Back').forEach(button => {
   button.addEventListener('click', () => {
